f725f6b4abf1e4649612861b6b12176dafc99a3b
Aditya Sarawgi <aditya@uber.com>
2013-10-21 22:07:50 -0700

    first commit

---
+++ gen-py/user_profile/test_client.py
@@ -0,0 +1,27 @@
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+from ttypes import UserProfile
+import UserStorage
+
+transport = TSocket.TSocket("localhost", 9090)
+
+# Buffering is critical. Raw sockets are very slow
+transport = TTransport.TBufferedTransport(transport)
+
+# Connect
+transport.open()
+
+protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+service = UserStorage.Client(protocol)
+
+
+up = UserProfile(uid=1,
+                 name="Test User",
+                 blurb="Thrift is great")
+
+service.store(up)
+
+transport.close()
